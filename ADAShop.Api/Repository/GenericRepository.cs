45e3293d7e0f9030235d4a3654fecde55bd14f8b
joann.quintero <joann.quintero@kpgin.com>
2025-07-03 21:59:20 -0500

    feat: implementando clases GenericUnitOfWork y GenericRepository

---
+++ ADAShop.Api/Repository/GenericRepository.cs
@@ -0,0 +1,155 @@
+﻿using ADAShop.Api.Data;
+using ADAShop.Api.Helpers;
+using ADAShop.Shared.DTOs;
+using ADAShop.Shared.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADAShop.Api.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly Context _context;
+        private readonly DbSet<T> _entity;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+            _entity = context.Set<T>();
+        }
+
+        public virtual async Task<Response<T>> AddAsync(T entity)
+        {
+            _context.Add(entity);
+            try
+            {
+                await _context.SaveChangesAsync();
+                return new Response<T>
+                {
+                    WasSuccess = true,
+                    Result = entity
+                };
+            }
+            catch (DbUpdateException)
+            {
+                return DbUpdateExceptionResponse();
+            }
+            catch (Exception exception)
+            {
+                return ExceptionResponse(exception);
+            }
+        }
+
+        public virtual async Task<Response<T>> DeleteAsync(int id)
+        {
+            var row = await _entity.FindAsync(id);
+            if (row == null)
+            {
+                return new Response<T>
+                {
+                    WasSuccess = false,
+                    Message = "Registro no encontrado"
+                };
+            }
+
+            try
+            {
+                _entity.Remove(row);
+                await _context.SaveChangesAsync();
+                return new Response<T>
+                {
+                    WasSuccess = true,
+                };
+            }
+            catch
+            {
+                return new Response<T>
+                {
+                    WasSuccess = false,
+                    Message = "No se puede borrar, porque tiene registros relacionados"
+                };
+            }
+        }
+
+        public virtual async Task<Response<T>> GetAsync(int id)
+        {
+            var row = await _entity.FindAsync(id);
+            if (row != null)
+            {
+                return new Response<T>
+                {
+                    WasSuccess = true,
+                    Result = row
+                };
+            }
+            return new Response<T>
+            {
+                WasSuccess = false,
+                Message = "Registro no encontrado"
+            };
+        }
+
+        public virtual async Task<Response<IEnumerable<T>>> GetAsync(PaginationDTO pagination)
+        {
+            var queryable = _entity.AsQueryable();
+            return new Response<IEnumerable<T>>
+            {
+                WasSuccess = true,
+                Result = await queryable
+                    .Paginate(pagination)
+                    .ToListAsync()
+            };
+        }
+
+        public virtual async Task<Response<int>> GetTotalPagesAsync(PaginationDTO pagination)
+        {
+            var queryable = _entity.AsQueryable();
+            double count = await queryable.CountAsync();
+            int totalPages = (int)Math.Ceiling(count / pagination.RecordsNumber);
+            return new Response<int>
+            {
+                WasSuccess = true,
+                Result = totalPages
+            };
+        }
+
+        public virtual async Task<Response<T>> UpdateAsync(T entity)
+        {
+            try
+            {
+                _context.Update(entity);
+                await _context.SaveChangesAsync();
+                return new Response<T>
+                {
+                    WasSuccess = true,
+                    Result = entity
+                };
+            }
+            catch (DbUpdateException)
+            {
+                return DbUpdateExceptionResponse();
+            }
+            catch (Exception exception)
+            {
+                return ExceptionResponse(exception);
+            }
+        }
+
+        private Response<T> ExceptionResponse(Exception exception)
+        {
+            return new Response<T>
+            {
+                WasSuccess = false,
+                Message = exception.Message
+            };
+        }
+
+        private Response<T> DbUpdateExceptionResponse()
+        {
+            return new Response<T>
+            {
+                WasSuccess = false,
+                Message = "Ya existe el registro que estas intentando crear."
+            };
+        }
+    }
+}
